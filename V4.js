950d51e464d5747660806c1a0eacce7971da85bc
Nikhil Yadav <156886832+NikhilYadav2000@users.noreply.github.com>
2024-05-12 14:09:23 +0530

    Add files via upload

---
+++ V4.js
@@ -0,0 +1,52 @@
+// let nm ="33abc";
+// console.log(typeof nm); // string
+// console.log(nm); // abc
+// let strInNum=Number(nm); 
+// console.log(typeof strInNum); // number
+// console.log(strInNum); // NaN
+
+// let nm2 =null;
+// console.log(typeof nm2); // object
+// console.log(nm2); // null
+// let strInNum2=Number(nm2); // number
+// console.log(typeof strInNum2); 
+// console.log(strInNum2); // 0
+
+// let nm3 =undefined;
+// console.log(typeof nm3); // undefined
+// console.log(nm3); // undefined
+// let strInNum3=Number(nm3); 
+// console.log(typeof strInNum3); // number
+// console.log(strInNum3); // NaN
+
+// let nm4 =true;
+// console.log(typeof nm4); // console.log(typeof nm5); // boolean
+
+// console.log(nm4); // true
+// let strInNum4=Number(nm4); // number
+// console.log(typeof strInNum4); // number
+// console.log(strInNum4); // 1
+
+// let nm5 =false;
+// console.log(typeof nm5); // boolean
+// console.log(nm5); // false
+// let strInNum5=Number(nm5); // number
+// console.log(typeof strInNum5); // number
+// console.log(strInNum5); // 1
+
+let isLoggedIn=1;
+let boolInNum=Boolean(isLoggedIn);
+console.log(boolInNum); // true
+
+let isLoggedIn2="";
+let boolInNum2=Boolean(isLoggedIn2);
+console.log(boolInNum2); // false
+
+let isLoggedIn3="Nikhil";
+let boolInNum3=Boolean(isLoggedIn3);
+console.log(boolInNum3); // true
+
+let num="13";
+let strlInNum5=String(num);
+console.log(strlInNum5); // 13
+console.log(typeof strlInNum5); // string
