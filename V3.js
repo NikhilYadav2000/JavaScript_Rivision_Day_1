cc353dce231d8ee18131c86e8ecc171618dac221
Nikhil Yadav <156886832+NikhilYadav2000@users.noreply.github.com>
2024-05-12 13:47:19 +0530

    Add files via upload

---
+++ V3.js
@@ -0,0 +1,22 @@
+"use strict" // treat all JS code as newer version
+// alert(3+3); // we are using nodejs, not browser
+
+// Datatypes
+// 1. Primitive
+let name="Nikhil"
+let age="23"
+let isLoggedIn=true;
+// Number number=2 to the power 53
+// bigint
+// String=""
+// boolean=true/false
+// null is a stand alone value but also a datatype
+// null means the variable is empty
+// undefiend // value has not been assigned yet
+// symbol to define uniqueness
+
+// Object
+console.log(typeof(13)); // number
+console.log(typeof(name)); // string
+console.log(typeof(null)); // object(datatype)
+console.log(typeof(undefined)); // undefined(dataype)
